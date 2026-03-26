043584deb6cc1fc10330b8c294c7c8058180bea7
sohiofficial <soham5122004@gmail.com>
2026-03-26 09:58:20 +0530

    made the code deployment friendly adding .env and .gitignore for secrets and not adding the .env file to the git hub repo

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const {mongoDbConnect}= require("./connection.js");
@@ -6,10 +8,10 @@ const cookieParser = require("cookie-parser")
 const urlRouter = require("./routes/url.js");
 const staticRouter = require("./routes/staticRoute.js");
 const userRouter = require("./routes/users.js");
-const PORT= 8001;
+const PORT = process.env.PORT || 8001;
 const path = require("path");
 
-mongoDbConnect("mongodb://127.0.0.1:27017/lilUrl");
+mongoDbConnect(process.env.MONGODB_URI);
 
 app.set("view engine", "ejs");
 app.set("views",path.resolve("./views"));
